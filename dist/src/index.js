fd97c9c06ac237d9ed438b20e75f38cd8fa26de0
touma0dev <lucashenr429@gmai.com>
2023-02-14 02:50:49 -0300

    Signed-off-by: touma0dev <lucashenr429@gmai.com>

--- dist/src/index.js
+++ dist/src/index.js
@@ -1,4 +1,3 @@
-const path = require('path');
 const express = require('express');
 const app = express();
 const fs = require('fs');
@@ -7,36 +6,17 @@ const knex = require('knex')({
   connection: {
     host : 'raja.db.elephantsql.com',
     user : 'flrwydvq',
-    password : 'GR-lX238UqFEWMRCNCVPZQ4WmZs3dBOY',
+    password : `GR-lX238UqFEWMRCNCVPZQ4WmZs3dBOY`,
     database : 'flrwydvq'
   }
 });
-
-// Serve the files in the `dist` folder as static files
-app.use(express.static(path.join(__dirname, 'dist')));
-
-// Route to serve the JSON data file
-app.get('/data', async (req, res) => {
-  try {
-    const rows = await knex.select().from('processor');
-    res.json(rows);
-  } catch (err) {
-    console.error(err);
-    res.status(500).send('Error retrieving data');
-  }
-});
-
-// Periodically update the JSON data file
-async function updateFile() {
-  try {
-    const rows = await knex.select().from('processor');
-    fs.writeFile('./dist/data.json', JSON.stringify(rows), (err) => {
-      if (err) throw err;
-      console.log('File updated successfully');
-    });
-  } catch (err) {
-    console.error(err);
-  }
+module.exports = app;async function updateFile() {
+  const rows = await knex.select().from('processor');
+  fs.writeFile('./dist/data.json', JSON.stringify(rows), (err) => {
+    if (err) throw err;
+    console.log(rows)
+    console.log('File updated successfully');
+  });
 }
 
 async function monitorTable() {
@@ -48,13 +28,7 @@ async function monitorTable() {
       updateFile();
       previousRows = currentRows;
     }
-  }, 4000);
+  }, 2000);
 }
-
 monitorTable();
 
-// Start the server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
-});
